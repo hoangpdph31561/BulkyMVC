41dbbe53ec7c49d7c8c1085f9fb4a457feca275f
Hoàng Phạm Đức <hoang23577@gmail.com>
2024-03-15 10:27:41 +0700

    Create DB, and data to view

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Drink", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Food", DisplayOrder = 2 });
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}

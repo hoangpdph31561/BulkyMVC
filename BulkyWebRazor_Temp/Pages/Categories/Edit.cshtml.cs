8e0947fb010c401dc9536c66de4e5e034c8a5980
Hoàng Phạm Đức <hoang23577@gmail.com>
2024-03-16 10:48:47 +0700

    CRUD Razor Project (Just for learning)

---
+++ BulkyWebRazor_Temp/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,43 @@
+using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public Category? Category { get; set; }
+        public async Task<IActionResult> OnGet(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category = await _db.Categories.FirstOrDefaultAsync(u => u.Id == id);
+            if (Category == null)
+            {
+                return NotFound();
+            }
+            return Page();
+
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}

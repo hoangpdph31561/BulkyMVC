0b8f217c862c97929f771c6a276ecc7ea124b3dd
Hoàng Phạm Đức <hoang23577@gmail.com>
2024-03-16 10:16:42 +0700

    MVC with razor

---
+++ BulkyWebRazor_Temp/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BulkyWebRazor_Temp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWebRazor_Temp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+               new Category { Id = 1, Name = "Drink", DisplayOrder = 1 },
+               new Category { Id = 2, Name = "Food", DisplayOrder = 2 });
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+
+}
---
+++ BulkyWebRazor_Temp/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWebRazor_Temp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(1, 200, ErrorMessage = "Display Order for category must be between 1 and 200")]
+        public int DisplayOrder { get; set; }
+    }
+}

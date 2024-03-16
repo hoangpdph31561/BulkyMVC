0b608715ef5d97b3e4d918fbcbb4ddf60c785ddd
Hoàng Phạm Đức <hoang23577@gmail.com>
2024-03-16 17:58:32 +0700

    Generic Respository

---
+++ Bulky.DataAccess/Respository/IRespository/IRespository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Bulky.DataAccess.Respository.IRespository
+{
+    public interface IRespository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        void Remove(T entity);
+        void DeleteRange(IEnumerable<T> entity);
+    }
+}
---
+++ Bulky.DataAccess/Respository/Respository.cs
@@ -0,0 +1,45 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Respository.IRespository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Bulky.DataAccess.Respository
+{
+    public class Respository<T> : IRespository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> _dbSet;
+        public Respository(ApplicationDbContext db)
+        {
+            _db = db;
+            this._dbSet = _db.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            _dbSet.Add(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entity)
+        {
+            _dbSet.RemoveRange(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> querry = _dbSet;
+            querry = querry.Where(filter);
+            return querry.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> querry = _dbSet;
+            return querry.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+}
